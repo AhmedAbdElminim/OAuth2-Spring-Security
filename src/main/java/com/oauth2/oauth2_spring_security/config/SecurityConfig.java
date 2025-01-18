ddac9bc8c67912d366de5d794070b3d7d4b8b3a7
ahmedabdelminim <ahmedabdelminim@gmail.com>
2025-01-18 09:45:31 +0200

    initial commit

--- src/main/java/com/oauth2/oauth2_spring_security/config/SecurityConfig.java
+++ src/main/java/com/oauth2/oauth2_spring_security/config/SecurityConfig.java
@@ -1,5 +1,29 @@
 package com.oauth2.oauth2_spring_security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(request -> {
+            request.requestMatchers("/", "/login")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated();
+        }).oauth2Login(oauth2Login -> {
+            oauth2Login.loginPage("/login")
+                    .defaultSuccessUrl("/home",true);
+        });
+
+        return http.build();
+
+    }
 
 }
--- src/main/java/com/oauth2/oauth2_spring_security/entity/User.java
+++ src/main/java/com/oauth2/oauth2_spring_security/entity/User.java
@@ -1,5 +1,14 @@
 package com.oauth2.oauth2_spring_security.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
 public class User {
+    private String name;
+    private String email;
 
 }
